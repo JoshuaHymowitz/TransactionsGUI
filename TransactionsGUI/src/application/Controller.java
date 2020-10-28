80ffc87eae31a21d989e4aecfe6b1cdea5a4395e
MLHLocalhost <localhost@mlh.io>
2020-10-27 23:45:03 -0400

    Added close account stuff

--- TransactionsGUI/src/application/Controller.java
+++ TransactionsGUI/src/application/Controller.java
@@ -17,6 +17,7 @@ import javafx.scene.control.Alert.AlertType;
 public class Controller {
 	
 	private AccountDatabase accountDatabase = new AccountDatabase();
+	Date tempDate = new Date("1/1/2000"); // used for making accounts to be compared, the date is not checked
 	
 	@FXML
 	private TextArea messageArea;
@@ -25,16 +26,25 @@ public class Controller {
 	private TextField openingDeposit, openingFirstName, openingLastName;
 	
 	@FXML
+	private TextField closingFirstName, closingLastName;
+	
+	@FXML
 	private TextField openDay, openMonth, openYear;
 	
 	@FXML
 	private RadioButton openChecking, openSavings, openMoneyMarket;
 	
 	@FXML
+	private RadioButton closeChecking, closeSavings, closeMoneyMarket;
+	
+	@FXML
 	private CheckBox openLoyal, openDirectDeposit;
 	
 	@FXML
 	private Button openAccount;
+	
+	@FXML
+	private Button closeAccount;
 	
 	@FXML
 	/**
@@ -146,14 +156,15 @@ public class Controller {
 		else {
 			messageArea.appendText("Select an account type\n");
 		}
+		accountDatabase.printAccounts();
 	}
 		
 	@FXML
 	/**
-	 * Event handler for the checking radio button
+	 * Event handler for the open checking radio button
 	 * @param event
 	 */
-	void openCheckingButtonPress(ActionEvent e) {
+	void openCheckingButtonPress(ActionEvent event) {
 		openSavings.setSelected(false);
 		openMoneyMarket.setSelected(false);
 		
@@ -164,10 +175,10 @@ public class Controller {
 	
 	@FXML
 	/**
-	 * Event handler for the savings radio button
+	 * Event handler for the open savings radio button
 	 * @param event
 	 */
-	void openSavingsButtonPress(ActionEvent e) {
+	void openSavingsButtonPress(ActionEvent event) {
 		openChecking.setSelected(false);
 		openMoneyMarket.setSelected(false);
 		
@@ -178,10 +189,10 @@ public class Controller {
 	
 	@FXML
 	/**
-	 * Event handler for the money market radio button
+	 * Event handler for the open money market radio button
 	 * @param event
 	 */
-	void openMoneyMarketButtonPress(ActionEvent e) {
+	void openMoneyMarketButtonPress(ActionEvent event) {
 		openChecking.setSelected(false);
 		openSavings.setSelected(false);
 		
@@ -190,6 +201,99 @@ public class Controller {
 		openDirectDeposit.setSelected(false);
 		openDirectDeposit.setDisable(true);
 	}
+	
+	@FXML
+	void closeAccount(ActionEvent event) {
+		// get account holder name
+		String firstName;
+		String lastName;
+		try {
+			firstName = closingFirstName.getText();
+			lastName = closingLastName.getText();
+		}
+		catch (Exception e) {
+			messageArea.appendText("Make sure to enter a first and last name\n");
+			return;
+		}
+		
+		if(closeChecking.isSelected()) {
+			Profile checkingDeleteProfile = new Profile(firstName, lastName);
+			
+			Checking deleteChecking = new Checking(checkingDeleteProfile,1,tempDate, true);
+			boolean checkingRemovability = accountDatabase.remove(deleteChecking);
+			
+			if(!checkingRemovability) {
+				messageArea.appendText("Account does not exist.\n");
+			}
+			else {
+				messageArea.appendText("Account closed and removed from the database\n");
+			}
+			
+		}
+		else if(closeSavings.isSelected()) {
+			Profile savingsDeleteProfile = new Profile(firstName,lastName);
+			
+			Savings deleteSavings = new Savings(savingsDeleteProfile,1,tempDate, true);
+			boolean savingsRemovability = accountDatabase.remove(deleteSavings);
+			
+			if(!savingsRemovability) {
+				messageArea.appendText("Account does not exist.\n");
+			}
+			else {
+				messageArea.appendText("Account closed and removed from the database\n");
+			}
+			
+		}
+		else if(closeMoneyMarket.isSelected()) {
+			Profile moneyDeleteProfile = new Profile(firstName,lastName);
+
+			MoneyMarket deleteMoney = new MoneyMarket(moneyDeleteProfile,1,tempDate, 1);
+			boolean moneyMarketRemovability = accountDatabase.remove(deleteMoney);
+			
+			if(!moneyMarketRemovability) {
+				messageArea.appendText("Account does not exist.\n");
+			}
+			else {
+				messageArea.appendText("Account closed and removed from the database\n");
+			}
+		}
+		else {
+			messageArea.appendText("Select an account type\n");
+		}
+		accountDatabase.printAccounts();
+	}
+	
+	@FXML
+	/**
+	 * Event handler for the open checking radio button
+	 * @param event
+	 */
+	void closeCheckingButtonPress(ActionEvent event) {
+		closeSavings.setSelected(false);
+		closeMoneyMarket.setSelected(false);
+	}
+	
+	@FXML
+	/**
+	 * Event handler for the close savings radio button
+	 * @param event
+	 */
+	void closeSavingsButtonPress(ActionEvent event) {
+		closeChecking.setSelected(false);
+		closeMoneyMarket.setSelected(false);
+	}
+	
+	@FXML
+	/**
+	 * Event handler for the close money market radio button
+	 * @param event
+	 */
+	void closeMoneyMarketButtonPress(ActionEvent event) {
+		closeChecking.setSelected(false);
+		closeSavings.setSelected(false);
+	}
+	
+	
 		
 
 		
