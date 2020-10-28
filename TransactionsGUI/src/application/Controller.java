9de2bb011481e214754f5641770dd2824643d372
MLHLocalhost <localhost@mlh.io>
2020-10-27 22:39:17 -0400

    Implemented open account stuff, initialized account database in controller

--- TransactionsGUI/src/application/Controller.java
+++ TransactionsGUI/src/application/Controller.java
@@ -3,7 +3,6 @@ package application;
 import java.io.File;
 import TransactionManagement.*;
 
-
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
@@ -17,11 +16,16 @@ import javafx.scene.control.Alert.AlertType;
 
 public class Controller {
 	
+	private AccountDatabase accountDatabase = new AccountDatabase();
+	
 	@FXML
 	private TextArea messageArea;
 	
 	@FXML
-	private TextField openingDeposit, openingName;
+	private TextField openingDeposit, openingFirstName, openingLastName;
+	
+	@FXML
+	private TextField openDay, openMonth, openYear;
 	
 	@FXML
 	private RadioButton openChecking, openSavings, openMoneyMarket;
@@ -35,26 +39,163 @@ public class Controller {
 	@FXML
 	/**
 	 * Event handler for the open account button
+	 * @param event
 	 */
 	void openAccount(ActionEvent event) {
 		
+		// get initial deposit amount
+		double openDepositAmount;
 		try {
-			double depositAmount = Double.parseDouble(openingDeposit.getText());
-			//messageArea.appendText(String.valueOf(depositAmount));
-			System.out.println("Success");
-			
+			openDepositAmount = Double.parseDouble(openingDeposit.getText());
 		}
-		
 		catch (Exception e) {
-			//messageArea.appendText("Make sure to enter a number in the initial deposit box");
-			System.out.println("Exception");
+			messageArea.appendText("Make sure to enter a number value for initial deposit\n");
+			return;
 		}
 		
+		// get account holder name
+		String firstName;
+		String lastName;
+		try {
+			firstName = openingFirstName.getText();
+			lastName = openingLastName.getText();
+		}
+		catch (Exception e) {
+			messageArea.appendText("Make sure to enter a first and last name\n");
+			return;
+		}
 		
+		if(openChecking.isSelected()) {
+			Profile checkingsNewProfile = new Profile(firstName, lastName);
+			
+			String dateString = openDay.getText() + "/" + openMonth.getText() + "/" + openYear.getText();
+			Date checkingsDateOpened = new Date(dateString);
+			if(!checkingsDateOpened.isValid()) {
+				messageArea.appendText("Date is invalid\n");
+				return;
+			}
+			
+			boolean directDeposit;
+			if(openDirectDeposit.isPressed()) {
+				directDeposit = true;
+			}
+			else {
+				directDeposit = false;
+			}
+
+			
+			Checking newChecking = new Checking(checkingsNewProfile, openDepositAmount,checkingsDateOpened, directDeposit);
+			
+			boolean checkingAddable = accountDatabase.add(newChecking);
+			
+			if(!checkingAddable) {
+				messageArea.appendText("Accountis already in the database.\n");
+			}
+			else {
+				messageArea.appendText("Account opened and added to the database\n");
+			}
+		}
+		else if(openSavings.isSelected()) {
+			Profile savingsNewProfile = new Profile(firstName, lastName);
+			
+			String dateString = openDay.getText() + "/" + openMonth.getText() + "/" + openYear.getText();
+			Date savingsDateOpened = new Date(dateString);
+			if(!savingsDateOpened.isValid()) {
+				messageArea.appendText("Date is invalid\n");
+				return;
+			}
+			
+			boolean isLoyal;
+			if(openLoyal.isPressed()) {
+				isLoyal = true;
+			}
+			else {
+				isLoyal = false;
+			}
+			
+			Savings newSavings = new Savings(savingsNewProfile,openDepositAmount, savingsDateOpened, isLoyal);
+			boolean savingsAddable = accountDatabase.add(newSavings);
+			
+			if(!savingsAddable) {
+				messageArea.appendText("Account is already in the database.\n");
+			}
+			else {
+				messageArea.appendText("Account opened and added to the database\n");
+			}
+		}
+		else if(openMoneyMarket.isSelected()) {
+			Profile moneyNewProfile = new Profile(firstName,lastName);
+			
+			String dateString = openDay.getText() + "/" + openMonth.getText() + "/" + openYear.getText();
+			Date moneyDateOpened = new Date(dateString);
+			if(!moneyDateOpened.isValid()) {
+				messageArea.appendText("Date is invalid\n");
+				return;
+			}
+			
+			MoneyMarket newMoneyMarket = new MoneyMarket(moneyNewProfile,openDepositAmount, moneyDateOpened, 0);
+			boolean moneyMarketAddable = accountDatabase.add(newMoneyMarket);
+			
+			if(!moneyMarketAddable) {
+				messageArea.appendText("Account is already in the database.\n");
+			}
+			else {
+				messageArea.appendText("Account opened and added to the database\n");
+			}
+		}
+		else {
+			messageArea.appendText("Select an account type\n");
+		}
+	}
+		
+	@FXML
+	/**
+	 * Event handler for the checking radio button
+	 * @param event
+	 */
+	void openCheckingButtonPress(ActionEvent e) {
+		openSavings.setSelected(false);
+		openMoneyMarket.setSelected(false);
+		
+		openDirectDeposit.setDisable(false);
+		openLoyal.setSelected(false);
+		openLoyal.setDisable(true);
 	}
 	
+	@FXML
+	/**
+	 * Event handler for the savings radio button
+	 * @param event
+	 */
+	void openSavingsButtonPress(ActionEvent e) {
+		openChecking.setSelected(false);
+		openMoneyMarket.setSelected(false);
+		
+		openLoyal.setDisable(false);
+		openDirectDeposit.setSelected(false);
+		openDirectDeposit.setDisable(true);
+	}
 	
-	
-	
-	
+	@FXML
+	/**
+	 * Event handler for the money market radio button
+	 * @param event
+	 */
+	void openMoneyMarketButtonPress(ActionEvent e) {
+		openChecking.setSelected(false);
+		openSavings.setSelected(false);
+		
+		openLoyal.setSelected(false);
+		openLoyal.setDisable(true);
+		openDirectDeposit.setSelected(false);
+		openDirectDeposit.setDisable(true);
+	}
+		
+
+		
 }
+	
+	
+	
+	
+	
